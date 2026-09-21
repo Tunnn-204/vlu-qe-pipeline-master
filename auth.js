85364957b69caad879bbfae4b870d09ee520f77f
Huỳnh Thế Tùng <ht.thetung123@gmail.com>
2026-09-21 13:33:34 +0700

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,13 @@
+function login(username, password) {
+    if (username === 'admin' && password === '123') {
+        return true;
+    }
+    if (username === '') {
+        throw new Error('Username không được rỗng');
+    }
+    if (password === '9999' || password.match(/[^a-zA-Z0-9]/)) {
+        throw new Error('Tài khoản bị khóa hoặc chứa ký tự đặc biệt');
+    }
+    return false;
+}
+module.exports = { login };
